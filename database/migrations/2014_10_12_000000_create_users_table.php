3b9a1821cb27ecb9b340be07804c93229daafa9e
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2023-02-25 11:30:04 +0700

    Table Users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,10 +17,15 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('phone_number');
+            $table->string('avatar')->nullable();
+            $table->enum('roles', ['admin', 'member']);
             $table->timestamps();
+
+
+//            $table->timestamp('email_verified_at')->nullable();
+//            $table->rememberToken();
         });
     }
 
