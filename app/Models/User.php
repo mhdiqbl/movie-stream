84cd45707b84853467a0b6544e559f958ff7acbd
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2023-02-25 14:24:58 +0700

    Add model movie, package, transaction

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone_number',
+        'avatar',
+        'roles',
     ];
 
     /**
@@ -30,7 +33,7 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
+//        'remember_token',
     ];
 
     /**
