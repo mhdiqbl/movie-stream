2ed90a228dc018ad698300d24165690ea19b04a4
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2023-02-25 14:26:45 +0700

    Add model user premium and edit model movie

--- app/Models/UserPremium.php
+++ app/Models/UserPremium.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class UserPremium extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'packages_id',
+        'user_id',
+        'end_of_subscription',
+    ];
 }
