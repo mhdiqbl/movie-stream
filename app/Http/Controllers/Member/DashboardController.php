52916ea058132e3cea6c3c0f7e6909d7e986e5ca
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2023-03-02 15:27:01 +0700

    Show Movie in dashboard page

--- app/Http/Controllers/Member/DashboardController.php
+++ app/Http/Controllers/Member/DashboardController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers\Member;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('member.dashboard');
+        $movies = Movie::orderBy('featured', 'DESC')->orderBy('created_at', 'DESC')->get();
+        return view('member.dashboard', compact('movies'));
     }
 
 }
