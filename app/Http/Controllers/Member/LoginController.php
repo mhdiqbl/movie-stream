dbfd9a22be7e3b8307835985823028708ef3a5ae
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2023-03-02 14:40:08 +0700

    Login Member

--- app/Http/Controllers/Member/LoginController.php
+++ app/Http/Controllers/Member/LoginController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Member;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -12,9 +14,19 @@ class LoginController extends Controller
         return view('member.auth');
     }
 
-    public function auth()
+    public function auth(LoginRequest $request)
     {
+        $credentials = $request->only('email', 'password');
+        $credentials['roles'] = 'member';
 
+        if (Auth::attempt($credentials)){
+            $request->session()->regenerate();
+
+            return redirect()->route('member-dashboard');
+        }
+        return back()->withErrors([
+            'credentials' => 'Your credential is invalid'
+        ])->withInput();
     }
 
     public function logout()
