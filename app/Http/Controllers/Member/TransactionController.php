3b2806b3c5a797f4a6d47c48ad86493008c96eaa
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2023-03-05 15:27:01 +0700

    Install package midtrans

---
+++ app/Http/Controllers/Member/TransactionController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Member;
+
+use App\Http\Controllers\Controller;
+use App\Models\Package;
+use App\Models\Transaction;
+
+use Illuminate\Http\Request;use Illuminate\Support\Str;
+
+class TransactionController extends Controller
+{
+    public function store(Request $request)
+    {
+        return 1;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Member\LoginController as MemberLoginController;
 use App\Http\Controllers\Member\DashboardController;
 use App\Http\Controllers\Member\MovieController as MemberMovieController;
 use App\Http\Controllers\Member\PricingController;
+use App\Http\Controllers\Member\TransactionController as MemberTransactionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -54,8 +55,9 @@ Route::post('/login', [MemberLoginController::class, 'auth'])->name('member-logi
 
 Route::group(['prefix' => 'member', 'middleware' => ['auth']], function(){
    Route::get('/', [DashboardController::class, 'index'])->name('member-dashboard');
-   Route::get('movie/{id}', [MemberMovieController::class, 'show'])->name('member-movie-detail');
     Route::get('/logout', [MemberLoginController::class, 'logout'])->name('member-logout');
+
+    Route::post('/transaction', [MemberTransactionController::class, 'store'])->name('member-transaction-store');
+
+   Route::get('movie/{id}', [MemberMovieController::class, 'show'])->name('member-movie-detail');
 });
-
-
