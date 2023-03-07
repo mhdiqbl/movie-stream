242bb82a3b10623975cfccd577fa378b9d6cfbfa
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2023-03-07 11:22:05 +0700

    add user premium controller

---
+++ app/Http/Controllers/Member/UserPremiumController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Member;
+
+use App\Http\Controllers\Controller;
+use App\Models\UserPremium;
+use Illuminate\Http\Request;
+
+class UserPremiumController extends Controller
+{
+    public function index()
+    {
+        $userId = auth()->user()->id;
+        $userPremium = UserPremium::with('package')
+            ->where('user_id', $userId)->first();
+
+        if (!$userPremium){
+            return redirect()->route('pricing');
+        }
+
+//        dd($userPremium);
+
+        return view('member.subscription', compact('userPremium'));
+    }
+}
